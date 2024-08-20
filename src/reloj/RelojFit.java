0a146874d81aace652a7910d273727271e685343
FacuMartinGarcia <estudiodesarrolladorsoftware@gmail.com>
2024-08-20 10:53:05 -0300

    variable y en metodo
    se agregó

--- src/reloj/RelojFit.java
+++ src/reloj/RelojFit.java
@@ -5,7 +5,7 @@ import reloj.Reloj;
 
 public class RelojFit extends Reloj {
 
-    public void cuentaPasos(int x) {
+    public void cuentaPasos(int x, int y) {
     }
 
     public void frecuenciaAleatoria() {
