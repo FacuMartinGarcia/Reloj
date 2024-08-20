4103b0a0e89a80aefe5dbf5620e95a7ce6f2aa42
RafaCod0011 <nicolascuello12@gmail.com>
2024-08-20 19:54:17 -0300

    UpdateMain
    V2

--- src/reloj/MainReloj.java
+++ src/reloj/MainReloj.java
@@ -13,19 +13,17 @@ public class MainReloj {
 
     public static void main(String[] args) {
         
-    //LocalDate fecha=LocalDate.of(2024,12,14);
-//    Reloj casio = new Reloj(LocalDate.now(), LocalTime.now(), "Pepe", 12354);
-//    
-//
-//    Persona person = new Persona("Juan", "Perez", 43, 1.75, casio);
-//    person.decirHora();
-//    
-//    casio.incrementarDia(5);
-//    casio.incrementarHora(10);
-//    
-//    person.decirHora();
     
-    RelojFit appleWatch=new RelojFit(LocalDate.now(), LocalTime.now(), "verde",12360);
+     Reloj casio = new Reloj(LocalDate.now(), LocalTime.now(), "Pepe", 12354);
+
+    Persona person = new Persona("Juan", "Perez", 43, 1.75, casio);
+     person.decirHora();
+     casio.incrementarDia(5);
+     casio.incrementarHora(10);
+
+     person.decirHora();
+    
+    RelojFit appleWatch =new RelojFit(LocalDate.now(), LocalTime.now(), "verde",12360);
     
     appleWatch.cuentaPasos(10, 8);
         
